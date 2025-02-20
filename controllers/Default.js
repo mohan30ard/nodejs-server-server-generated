b94b019f76910954654f7d44f7c9c48ac42be232
Mohan <n01620006@humber.ca>
2025-02-19 23:10:34 -0500

    swagger

---
+++ controllers/Default.js
@@ -0,0 +1,294 @@
+'use strict';
+
+var utils = require('../utils/writer.js');
+var Default = require('../service/DefaultService');
+
+module.exports.apiUsersUser_addresses1DELETE = function apiUsersUser_addresses1DELETE (req, res, next) {
+  Default.apiUsersUser_addresses1DELETE()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.apiUsersUser_addresses1GET = function apiUsersUser_addresses1GET (req, res, next) {
+  Default.apiUsersUser_addresses1GET()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.apiUsersUser_addresses1PUT = function apiUsersUser_addresses1PUT (req, res, next, body) {
+  Default.apiUsersUser_addresses1PUT(body)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.apiUsersUser_addressesGET = function apiUsersUser_addressesGET (req, res, next) {
+  Default.apiUsersUser_addressesGET()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.apiUsersUser_addressesPOST = function apiUsersUser_addressesPOST (req, res, next, body) {
+  Default.apiUsersUser_addressesPOST(body)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.apiUsersUser_order_items1DELETE = function apiUsersUser_order_items1DELETE (req, res, next) {
+  Default.apiUsersUser_order_items1DELETE()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.apiUsersUser_order_items1GET = function apiUsersUser_order_items1GET (req, res, next) {
+  Default.apiUsersUser_order_items1GET()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.apiUsersUser_order_items1PUT = function apiUsersUser_order_items1PUT (req, res, next, body) {
+  Default.apiUsersUser_order_items1PUT(body)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.apiUsersUser_order_itemsPOST = function apiUsersUser_order_itemsPOST (req, res, next, body) {
+  Default.apiUsersUser_order_itemsPOST(body)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.apiUsersUser_orders1DELETE = function apiUsersUser_orders1DELETE (req, res, next) {
+  Default.apiUsersUser_orders1DELETE()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.apiUsersUser_orders1GET = function apiUsersUser_orders1GET (req, res, next) {
+  Default.apiUsersUser_orders1GET()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.apiUsersUser_orders1PUT = function apiUsersUser_orders1PUT (req, res, next, body) {
+  Default.apiUsersUser_orders1PUT(body)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.apiUsersUser_ordersGET = function apiUsersUser_ordersGET (req, res, next) {
+  Default.apiUsersUser_ordersGET()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.apiUsersUser_ordersPOST = function apiUsersUser_ordersPOST (req, res, next, body) {
+  Default.apiUsersUser_ordersPOST(body)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.apiUsersUsersGET = function apiUsersUsersGET (req, res, next) {
+  Default.apiUsersUsersGET()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.apiUsersUsersPOST = function apiUsersUsersPOST (req, res, next, body) {
+  Default.apiUsersUsersPOST(body)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.apiVendorsCategories1GET = function apiVendorsCategories1GET (req, res, next) {
+  Default.apiVendorsCategories1GET()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.apiVendorsCategories2DELETE = function apiVendorsCategories2DELETE (req, res, next) {
+  Default.apiVendorsCategories2DELETE()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.apiVendorsCategories4PUT = function apiVendorsCategories4PUT (req, res, next, body) {
+  Default.apiVendorsCategories4PUT(body)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.apiVendorsCategoriesGET = function apiVendorsCategoriesGET (req, res, next) {
+  Default.apiVendorsCategoriesGET()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.apiVendorsCategoriesPOST = function apiVendorsCategoriesPOST (req, res, next, body) {
+  Default.apiVendorsCategoriesPOST(body)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.apiVendorsProducts2GET = function apiVendorsProducts2GET (req, res, next) {
+  Default.apiVendorsProducts2GET()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.apiVendorsProductsGET = function apiVendorsProductsGET (req, res, next) {
+  Default.apiVendorsProductsGET()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.apiVendorsProductsPOST = function apiVendorsProductsPOST (req, res, next, body) {
+  Default.apiVendorsProductsPOST(body)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.apiVendorsVendors1DELETE = function apiVendorsVendors1DELETE (req, res, next) {
+  Default.apiVendorsVendors1DELETE()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.apiVendorsVendors1GET = function apiVendorsVendors1GET (req, res, next) {
+  Default.apiVendorsVendors1GET()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.apiVendorsVendors1PUT = function apiVendorsVendors1PUT (req, res, next, body) {
+  Default.apiVendorsVendors1PUT(body)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.apiVendorsVendorsGET = function apiVendorsVendorsGET (req, res, next) {
+  Default.apiVendorsVendorsGET()
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
+
+module.exports.apiVendorsVendorsPOST = function apiVendorsVendorsPOST (req, res, next, body) {
+  Default.apiVendorsVendorsPOST(body)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
---
+++ service/DefaultService.js
@@ -0,0 +1,362 @@
+'use strict';
+
+
+/**
+ * Delete Address
+ *
+ * no response value expected for this operation
+ **/
+exports.apiUsersUser_addresses1DELETE = function() {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * Get Address by Id
+ *
+ * no response value expected for this operation
+ **/
+exports.apiUsersUser_addresses1GET = function() {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * Update Address
+ *
+ * body Object  (optional)
+ * no response value expected for this operation
+ **/
+exports.apiUsersUser_addresses1PUT = function(body) {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * Get address
+ *
+ * no response value expected for this operation
+ **/
+exports.apiUsersUser_addressesGET = function() {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * Add Address
+ *
+ * body Object  (optional)
+ * no response value expected for this operation
+ **/
+exports.apiUsersUser_addressesPOST = function(body) {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * Delete Order-item
+ *
+ * no response value expected for this operation
+ **/
+exports.apiUsersUser_order_items1DELETE = function() {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * Get Order-items by Id
+ *
+ * no response value expected for this operation
+ **/
+exports.apiUsersUser_order_items1GET = function() {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * Update Order-item
+ *
+ * body Object  (optional)
+ * no response value expected for this operation
+ **/
+exports.apiUsersUser_order_items1PUT = function(body) {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * Create Order-item
+ *
+ * body Object  (optional)
+ * no response value expected for this operation
+ **/
+exports.apiUsersUser_order_itemsPOST = function(body) {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * Delete Order
+ *
+ * no response value expected for this operation
+ **/
+exports.apiUsersUser_orders1DELETE = function() {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * Get Order by Id
+ *
+ * no response value expected for this operation
+ **/
+exports.apiUsersUser_orders1GET = function() {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * Update Order
+ *
+ * body Object  (optional)
+ * no response value expected for this operation
+ **/
+exports.apiUsersUser_orders1PUT = function(body) {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * Get Orders
+ *
+ * no response value expected for this operation
+ **/
+exports.apiUsersUser_ordersGET = function() {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * Create Order
+ *
+ * body Object  (optional)
+ * no response value expected for this operation
+ **/
+exports.apiUsersUser_ordersPOST = function(body) {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * Get Users
+ *
+ * no response value expected for this operation
+ **/
+exports.apiUsersUsersGET = function() {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * Register User
+ *
+ * body Object  (optional)
+ * no response value expected for this operation
+ **/
+exports.apiUsersUsersPOST = function(body) {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * Get Category By ID
+ *
+ * no response value expected for this operation
+ **/
+exports.apiVendorsCategories1GET = function() {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * Delete Category
+ *
+ * no response value expected for this operation
+ **/
+exports.apiVendorsCategories2DELETE = function() {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * Update Category
+ *
+ * body Object  (optional)
+ * no response value expected for this operation
+ **/
+exports.apiVendorsCategories4PUT = function(body) {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * Get Categories
+ *
+ * no response value expected for this operation
+ **/
+exports.apiVendorsCategoriesGET = function() {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * Add Category
+ *
+ * body Object  (optional)
+ * no response value expected for this operation
+ **/
+exports.apiVendorsCategoriesPOST = function(body) {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * Get Product by Id
+ *
+ * no response value expected for this operation
+ **/
+exports.apiVendorsProducts2GET = function() {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * Get Products
+ *
+ * no response value expected for this operation
+ **/
+exports.apiVendorsProductsGET = function() {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * Add Product
+ *
+ * body Object  (optional)
+ * no response value expected for this operation
+ **/
+exports.apiVendorsProductsPOST = function(body) {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * Delete Vendor
+ *
+ * no response value expected for this operation
+ **/
+exports.apiVendorsVendors1DELETE = function() {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * Get Vendor by Id
+ *
+ * no response value expected for this operation
+ **/
+exports.apiVendorsVendors1GET = function() {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * Update Vendor
+ *
+ * body Object  (optional)
+ * no response value expected for this operation
+ **/
+exports.apiVendorsVendors1PUT = function(body) {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * Get Vendors
+ *
+ * no response value expected for this operation
+ **/
+exports.apiVendorsVendorsGET = function() {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
+
+/**
+ * Register Vendor
+ *
+ * body Object  (optional)
+ * no response value expected for this operation
+ **/
+exports.apiVendorsVendorsPOST = function(body) {
+  return new Promise(function(resolve, reject) {
+    resolve();
+  });
+}
+
